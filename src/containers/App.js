e1cf2f4e91bc0fb70d0091d30d0921e803e7d3fa
Nathan Robbert <admin@bigrocketgames.com>
2017-09-06 21:32:56 -0700

    testing fetch.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import './App.css'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      sports: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://team-schedules.herokuapp.com/api/v1/sports')
+      .then(response => response.json())
+      .then(sports => this.setState({ sports }))
+  }
+
+
   render() {
+
+    console.log(this.state)
     return(
-      <div>
+      <div className="App">
+        {/* need to create sports container and import that for use here */}
         App container
       </div>
     );
