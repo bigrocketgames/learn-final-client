c17a3f6ea409d4a42ed01fd422d39601d6a3355d
Nathan Robbert <admin@bigrocketgames.com>
2017-09-06 20:48:24 -0700

    Setting up basic file for insuring linkage to index.js is working.

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class App extends Component {
+  render() {
+    return {
+      <Div>
+        App Container
+      </div>
+    }
+  }
+}
