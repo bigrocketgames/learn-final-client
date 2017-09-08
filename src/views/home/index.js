d9afd74b6bbdd4cc2df2099ee2f8aa1b497e3f3e
Nathan Robbert <admin@bigrocketgames.com>
2017-09-08 10:51:43 -0700

    Added view for '/'

---
+++ src/views/home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="home">
+        <h3>Team Schedules Home Page</h3>
+      </div>
+    )
+  }
+
+}
+
+export default Home
