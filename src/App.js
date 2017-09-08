77f4fcc8c45ff53606b640aa97ec64537f314370
Nathan Robbert <admin@bigrocketgames.com>
2017-09-08 10:51:09 -0700

    Moved app back to main src file and added navbar links setup react router switching and inlcuded aphrodite css.

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink
+} from 'react-router-dom';
+import { StyleSheet, css } from 'aphrodite';
+import './App.css'
+import Signup from './views/signup';
+import Home from './views/home';
+
+// const API_URL = process.env.REACT_APP_API_URL;
+
+const styles = StyleSheet.create({
+  navbar: {
+    width: '100%',
+    background: '#18121e',
+    padding: '6px',
+  },
+
+  navlink: {
+    textDecoration: 'none',
+    padding: '6px',
+    color: 'white',
+    ':hover': {
+      background: '#fff',
+      color: '#18121e'
+    }
+  }
+})
+
+const NotFound = () => <div>NotFound</div>
+
+class App extends Component {
+
+  // constructor(props){
+  //   super(props)
+
+  //   this.state = {
+  //     sports: []
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   fetch(`${API_URL}/sports`)
+  //     .then(response => response.json())
+  //     .then(sports => this.setState({ sports }))
+  // }
+
+  render() {
+    console.log(this.state)
+    return(
+      <Router>
+        <div className="App">
+          <div className="navbar">
+            <div className={css(styles.navbar)}>
+              <NavLink className={css(styles.navlink)} to="/">Home</NavLink>
+              <NavLink className={css(styles.navlink)} to="/signup">Signup</NavLink>
+            </div>
+          </div>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
