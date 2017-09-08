f1ab9c5f04c361b07809134781aa1e0c8931e117
Nathan Robbert <admin@bigrocketgames.com>
2017-09-08 10:25:12 -0700

    Added auth module for redux.

---
+++ src/redux/modules/auth/actions.js
@@ -0,0 +1,46 @@
+import 'isomorphic-fetch';
+import { reset, SubmissionError } from 'redux-form';
+
+/* action creators */
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const authenticationRequest = () => {
+  return {
+    type: 'AUTHENTICATION_REQUEST'
+  }
+}
+
+export const setCurrentUser = user => {
+  return {
+    type: 'AUTHENTICATION_SUCCESS',
+    user
+  }
+}
+
+
+// Async actions - connect to Rails API
+
+export const signup = (userDetails, router) => {
+  return dispatch => {
+    dispatch(authenticationRequest())
+    return fetch('${API_URL}/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ user: userDetails })
+    })
+      .then(response => response.json())
+      .then(body => {
+        localStorage.setItem('team.schedule.token', body.token);
+        dispatch(setCurrentUser(body.user));
+        dispatch(reset('signup'));
+        router.history.replace('/home');
+      })
+    .catch(err => {
+      throw new SubmissionError(err);
+    })
+  }
+}
