e49362f4cdbe998e2833bc8f4fca46b3973e7675
Nathan Robbert <admin@bigrocketgames.com>
2017-09-08 10:22:47 -0700

    Getting redux setup.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+// custom module imports
 import './index.css';
+import store from './redux/store';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />, 
+  <Provider store={store}>
+    <App />
+  </Provider>, 
   document.getElementById('root')
 );
 
---
+++ src/redux/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { reducer as form } from 'redux-form';
+import auth from '../modules/auth/reducer';
+
+const reducers = combineReducers({
+  form,
+  auth,
+})
+const middleware = [thunk]
+
+export default createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(...middleware),
+)
