c993a3770eb3bb879fe645bb0a9a44bdfc825832
Nathan Robbert <admin@bigrocketgames.com>
2017-09-09 13:56:22 -0700

    Removing extra comment.

--- src/redux/modules/auth/reducer.js
+++ src/redux/modules/auth/reducer.js
@@ -1,5 +1,4 @@
 const initialState = {
-  // Adjust this here to check for token and set current user to the user associated with the token if there is one.
   isAuthenticated: false,
   isAuthenticating: true,
   currentUser: {}
