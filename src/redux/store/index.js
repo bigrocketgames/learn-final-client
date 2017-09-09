d71108adf0f1534c2845e0be8dfab297cc7f420e
Nathan Robbert <admin@bigrocketgames.com>
2017-09-08 22:32:19 -0700

    Adding persisted state and subscribing to the store to update it.

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,15 +1,28 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { reducer as form } from 'redux-form';
 import auth from '../modules/auth/reducer';
+import { loadState, saveState } from './localStorage';
+
+const persistedState = loadState();
 
 const reducers = combineReducers({
   form,
   auth,
 })
+
 const middleware = [thunk]
 
-export default createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(...middleware),
+const store = createStore(
+  reducers, persistedState,
+  compose (
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
