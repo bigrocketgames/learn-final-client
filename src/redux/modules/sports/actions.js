0b3b481b432e3dd38ee8bed4ab1630e7f5bde9ca
Nathan Robbert <admin@bigrocketgames.com>
2017-09-10 20:57:33 -0700

    Built out action  for handling when getting sports list from API.

--- src/redux/modules/sports/actions.js
+++ src/redux/modules/sports/actions.js
@@ -1,2 +1,24 @@
 import 'isomorphic-fetch';
-import { reset, SubmissionError } from 'redux-form';
+
+/* action creators */
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const getSportsSuccess = sports => {
+  return {
+    type: 'GET_SPORTS_SUCCESS',
+    sports: sports
+  }
+}
+
+
+// Async actions - connect to Rails API
+
+export const getSports = (router) => {
+  return dispatch => {
+    return fetch(`${API_URL}/sports`)
+      .then(response => response.json())
+      .then(sports => dispatch(getSportsSuccess(sports)))
+      .catch(error => console.log(error));
+  }
+}
