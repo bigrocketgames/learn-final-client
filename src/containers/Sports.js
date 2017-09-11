6099db2dd2fa2b24b3dc42a3957569f865105243
Nathan Robbert <admin@bigrocketgames.com>
2017-09-10 19:14:44 -0700

    Building sport component

--- src/containers/Sports.js
+++ src/containers/Sports.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import SportCard from '../components/SportCard';
+import { getSports } from '../redux/modules/sports/actions';
+
 class Sports extends Component {
 
+  componentDidMount() {
+    this.props.getSports()
+  }
+
+  render() {
+    return (
+      {this.props.sports.map(sport => <SportCard key={sport.id} sport={sport} />)}
+    );
+  }
 
 }
+
+const mapStateToProps = (State) => {
+  return ({
+    sports: state.sports
+  })
+}
+
+export default connect(mapStateToProps, {getSports})(Sports);
