38a70bccfc6b8397c6b3bb0b25f2c72dfad4e991
Nathan Robbert <admin@bigrocketgames.com>
2017-09-11 20:11:26 -0700

    Setting up admin page links.

--- src/views/admin/index.js
+++ src/views/admin/index.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import { Col, Row, Grid } from 'react-bootstrap';
 
 class Admin extends Component {
 
   
   render() {
-    return (
-      <div className="container">
-        <p>This will be the admin main page.</p>
-      </div>
-    )
+    if (localStorage.getItem('team.schedule.user_role') === "admin") {
+      return (
+        <div className="container">
+          <Grid>
+            <Row>
+              <Col sm={3} className="text-center">
+                <a href="/" className="adminLink">Users List</a>
+              </Col>
+              <Col sm={3} className="text-center">
+                <a href="/" className="adminLink">Sports List</a>
+              </Col>
+              <Col sm={3} className="text-center">
+                <a href="/" className="adminLink">Sub Sports List</a>
+              </Col>
+              <Col sm={3} className="text-center">
+                <a href="/" className="adminLink">Teams List</a>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
+      )
+    } else {
+      return (
+        <div className="container text-center">
+          <h2>YOU ARE NOT ALLOWED TO VIEW THIS AREA!!  GO AWAY!!</h2>
+        </div>
+      )
+    }
   }
 }
 
