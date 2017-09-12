ccd4f24d4aecfa78f5e13f3db159d121bc1df0aa
Nathan Robbert <admin@bigrocketgames.com>
2017-09-11 21:34:30 -0700

    Initial sports list component for admin area.

---
+++ src/views/admin/sportsList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SportsList extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <p>This will be the sports list.</p>
+      </div>
+    )
+  }
+}
+
+export default SportsList;
