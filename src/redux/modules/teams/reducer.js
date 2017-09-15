3c80f0d2eb55fa85963f8a65a6b541e85405a645
Nathan Robbert <admin@bigrocketgames.com>
2017-09-14 23:07:59 -0700

    Fixed reducer error.

--- src/redux/modules/teams/reducer.js
+++ src/redux/modules/teams/reducer.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
   switch(action.type) {
     case 'GET_TEAMS_SUCCESS':
-      return state.teams;
+      return action.teams;
 
     default:
       return state;
