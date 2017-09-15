19535f606113afff7ab9e1f26ac4fc146ca0f071
Nathan Robbert <admin@bigrocketgames.com>
2017-09-14 23:06:38 -0700

    Added action for handling GET /api/v1/teams

---
+++ src/redux/modules/teams/actions.js
@@ -0,0 +1,25 @@
+import 'isomorphic-fetch';
+
+/* action creators */
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const getTeamsSuccess = teams => {
+  debugger
+  return {
+    type: 'GET_TEAMS_SUCCESS',
+    teams: teams
+  }
+}
+
+// Async actions - connect to Rails API
+
+export const getTeams = (router) => {
+  return dispatch => {
+    return fetch(`${API_URL}/teams`)
+    .then(response => response.json())
+    .then(teams => dispatch(getTeamsSuccess(teams)))
+    .catch(error => console.log(error));
+  }
+
+}
