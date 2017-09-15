c5be0b2e6a49182d1e0e0498e126e46d6f2858c0
Nathan Robbert <admin@bigrocketgames.com>
2017-09-14 22:20:38 -0700

    Created UsersList and UsersListCard to render a table of all users for only admin users to see.

---
+++ src/components/admin/UsersListCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const UsersListCard = ({user}) => (
+  <tr>
+    <td>{user.id}</td>
+    <td>{user.email}</td>
+    <td><Button bsStyle="danger" bsSize="small">Delete User</Button></td>
+  </tr>
+)
+
+export default UsersListCard;
---
+++ src/containers/admin/UsersList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Table } from 'react-bootstrap';
+
+import UsersListCard from '../../components/admin/UsersListCard';
+import { getUsers } from '../../redux/modules/users/actions';
+
+class UsersList extends Component {
+
+  componentDidMount() {
+    this.props.getUsers();
+  }
+
+  render() {
+    return (
+      <div>
+        <Table striped bordered responsive hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Email</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.users.map(user => <UsersListCard key={user.id} user={user} />)}
+          </tbody>
+        </Table>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return ({
+    users: state.users
+  })
+}
+
+export default connect(mapStateToProps, {getUsers})(UsersList);
