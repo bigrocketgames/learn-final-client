21c823357ea2b90d35b5e344b4440ec179d82f15
Nathan Robbert <admin@bigrocketgames.com>
2017-09-15 18:51:15 -0700

    Displaying list of teams on admin/teams page.

--- src/components/admin/TeamsListCard.js
+++ src/components/admin/TeamsListCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const TeamsListCard = ({team}) => {
+const TeamsListCard = ({team}) => (
   <tr>
-  <td>{team.id}</td>
-  <td>{team.fullname}</td>
-  <td>{team.stadium_location}</td>
-  <td>TODO </td>
-  <td><Button bsStyle="danger" bsSize="small">Delete {team.fullname}</Button></td>
-</tr>
-}
+    <td>{team.id}</td>
+    <td>{team.fullname}</td>
+    <td>{team.stadium_location}</td>
+    <td>{team.sub_sport.name}</td>
+    <td><Button bsStyle="danger" bsSize="small">Delete {team.fullname}</Button></td>
+  </tr>
+)
 
 export default TeamsListCard;
