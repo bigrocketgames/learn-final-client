9db4cc4af1ea42cb101c7ddf39ab29038fe4f591
Nathan Robbert <admin@bigrocketgames.com>
2017-09-17 11:26:54 -0700

    Added Logout component for signing out the current user.

---
+++ src/views/logout/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { logout } from '../../redux/modules/auth/actions';
+
+class Logout extends Component {
+
+
+  componentDidMount() {
+    this.props.logout()
+  }
+
+  render() {
+    return (
+      <div>
+        You are signing out.
+      </div>
+    )
+  }
+}
+
+export default connect(null, { logout })(Logout)
