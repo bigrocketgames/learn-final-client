07029b5cf2aa59b83c798b93783fd5be88b4c504
Nathan Robbert <admin@bigrocketgames.com>
2017-09-17 21:22:48 -0700

    Created Schedule view Component.

---
+++ src/views/schedule/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Schedule extends Component {
+
+
+  render() {
+    return (
+      <div>
+        This will be a team's schedule.
+      </div>
+    )
+  }
+}
+
+export default Schedule
