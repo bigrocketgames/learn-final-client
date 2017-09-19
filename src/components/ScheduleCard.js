1fdc7e3751bd1dbbf2c34807e437414d9327495d
Nathan Robbert <admin@bigrocketgames.com>
2017-09-18 22:20:13 -0700

    Created scheduleCard component for rendering the schedule table data.

---
+++ src/components/ScheduleCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Table} from 'react-bootstrap';
+
+const ScheduleCard = (schedule) => {
+  return (
+    <Table bordered striped>
+      <thead>
+        <tr>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </Table>
+  )
+}
+
+export default ScheduleCard
