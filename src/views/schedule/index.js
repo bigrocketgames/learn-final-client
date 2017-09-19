19e824c186e6c7a7c2b63cf69770bba06d50ec74
Nathan Robbert <admin@bigrocketgames.com>
2017-09-18 22:13:02 -0700

    Initiated view for schedule page.

--- src/views/schedule/index.js
+++ src/views/schedule/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import Schedules from '../../containers/Schedules';
+
 class Schedule extends Component {
 
 
   render() {
+    const { match } = this.props
     return (
       <div>
-        This will be a team's schedule.
+        <Schedules match_url={`${match.url}`} match_params_id={`${match.params.id}`} />
       </div>
     )
   }
