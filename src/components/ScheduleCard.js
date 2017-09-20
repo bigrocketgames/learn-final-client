445df7fca3258437ea12e33975b8dfd2a4c872b1
Nathan Robbert <admin@bigrocketgames.com>
2017-09-19 21:32:36 -0700

    Formatted the date and time for the game.  Added an @ in between the away and home teams.

--- src/components/ScheduleCard.js
+++ src/components/ScheduleCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const ScheduleCard = (schedule) => {
+  const game_time = new Date(schedule.game.game_time);
+  const options = {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZoneName: 'short'}
   return (
     <tr>
-      <td>{new Date(schedule.game.game_time).toString()}</td>
+      <td>{game_time.toLocaleDateString('en-US', options)}</td>
       <td>{schedule.game.away_team.fullname}</td>
+      <td>@</td>
       <td>{schedule.game.home_team.fullname}</td>
       <td>{schedule.game.location}</td>
     </tr>
