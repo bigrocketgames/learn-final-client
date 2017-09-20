eaaf16814c18f2565fedad55173a2a11c000094d
Nathan Robbert <admin@bigrocketgames.com>
2017-09-19 21:46:15 -0700

    Changed date formatting to lookup the standard format based on where a person is located.

--- src/components/ScheduleCard.js
+++ src/components/ScheduleCard.js
@@ -5,7 +5,7 @@ const ScheduleCard = (schedule) => {
   const options = {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZoneName: 'short'}
   return (
     <tr>
-      <td>{game_time.toLocaleDateString('en-US', options)}</td>
+      <td>{game_time.toLocaleDateString("lookup", options)}</td>
       <td>{schedule.game.away_team.fullname}</td>
       <td>@</td>
       <td>{schedule.game.home_team.fullname}</td>
