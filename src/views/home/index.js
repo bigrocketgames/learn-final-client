35b816c313b2410427550d8bda9ad3d3b2314d0c
Nathan Robbert <admin@bigrocketgames.com>
2017-09-23 08:59:31 -0700

    What appears at top of page depends on if user is logged in or not.

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import UserTeams from '../../containers/UserTeams';
 
 class Home extends Component {
 
   render() {
+    let pageHeader = null;
+    if (this.props.user.id) {
+      pageHeader = <div>
+          <h3 className="text-center">Your Favorite Teams</h3>
+          <p className="text-center">Click them to see their schedule</p><hr/>
+          <UserTeams />
+          </div>
+    } else {
+      pageHeader = <div><h1 className="text-center">Team Schedules</h1><p className="text-center">Find out when your favorite team plays</p><hr /></div>
+    }
+
     return (
       <div className="container">
-        <h1 className="text-center">Team Schedules</h1>
-        <p className="text-center">Find out when your favorite team plays</p>
-        <hr />
-        <UserTeams />
+        {pageHeader}
       </div>
     )
   }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    user: state.auth.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Home);
