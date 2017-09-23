0767f0c589c717dbc0907728d7c226d9e967c4c9
Nathan Robbert <admin@bigrocketgames.com>
2017-09-22 23:42:56 -0700

    Added some text and connected to the UserTeams container.

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
+import UserTeams from '../../containers/UserTeams';
+
 class Home extends Component {
 
   render() {
     return (
       <div className="container">
-        <h2 className="text-center">Team Schedules</h2>
-        <h6 className="text-center">Find out when your favorite team plays</h6>
+        <h1 className="text-center">Team Schedules</h1>
+        <p className="text-center">Find out when your favorite team plays</p>
+        <hr />
+        <UserTeams />
       </div>
     )
   }
-
 }
 
 export default Home
