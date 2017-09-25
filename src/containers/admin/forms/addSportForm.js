cd45219b3c7af62c25111dda648189e9e0182999
Nathan Robbert <admin@bigrocketgames.com>
2017-09-24 22:30:36 -0700

    Sport form for adding a new sport.

---
+++ src/containers/admin/forms/addSportForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+import { addSport } from '../../../redux/modules/sports/actions';
+
+const handleAddSport = (values, dispatch) => {
+  dispatch(addSport(values))
+}
+
+let AddSportForm = props => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit(handleAddSport)}>
+      <div className="container">
+        <h2>Add a New Sport</h2>
+        <label htmlFor="name">Sport Name</label>
+        <div>
+          <Field name="name" component="input" type="text" placeholder="Sport"/>
+        </div>
+        <div>
+          <button type="submit" disabled={pristine || submitting}>
+            Add Sport
+          </button>
+          <button type="button" disabled={pristine || submitting} onClick={reset}>
+            Clear Form
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+AddSportForm = reduxForm({
+  form: 'addSport'
+})(AddSportForm)
+
+export default AddSportForm
