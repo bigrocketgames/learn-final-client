0f5e27e3bcf542897838b0b6738cdb467c8ae312
Nathan Robbert <admin@bigrocketgames.com>
2017-09-24 22:33:32 -0700

    Added action for adding sport and deleting sport.

--- src/redux/modules/sports/actions.js
+++ src/redux/modules/sports/actions.js
@@ -1,4 +1,5 @@
 import 'isomorphic-fetch';
+import { reset, SubmissionError } from 'redux-form';
 
 /* action creators */
 
@@ -14,11 +15,49 @@ export const getSportsSuccess = sports => {
 
 // Async actions - connect to Rails API
 
-export const getSports = (router) => {
+export const getSports = () => {
   return dispatch => {
     return fetch(`${API_URL}/sports`)
       .then(response => response.json())
       .then(sports => dispatch(getSportsSuccess(sports)))
       .catch(error => console.log(error));
+  }
+}
+
+export const addSport = (sportDetails) => {
+  return dispatch => {
+    return fetch(`${API_URL}/sports`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer: " + localStorage.getItem('team.schedule.token')
+      },
+      body: JSON.stringify({sport: sportDetails})
+    })
+      .then(response => response.json())
+      .then(sports => {
+        dispatch(getSportsSuccess(sports));
+        dispatch(reset('addSport'));
+      })
+      .catch(err => {
+        throw new SubmissionError(err);
+      })
+  }
+}
+
+export const deleteSport = (sportId) => {
+  return dispatch => {
+    return fetch(`${API_URL}/sports/${sportId}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer: " + localStorage.getItem('team.schedule.token')
+      }
+    })
+      .then(response => response.json())
+      .then(sports => dispatch(getSportsSuccess(sports)))
+      .catch(error => console.log(error))
   }
 }
