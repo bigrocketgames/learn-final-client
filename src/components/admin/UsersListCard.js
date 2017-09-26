0eec57155dfbe355800e42eaca928b6cf81593d4
Nathan Robbert <admin@bigrocketgames.com>
2017-09-25 20:23:15 -0700

    Converting admin bool to a role name to display on the users table.

--- src/components/admin/UsersListCard.js
+++ src/components/admin/UsersListCard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const UsersListCard = ({user}) => {
+  const role = user.admin ? "Admin" : "User"
+
   return (
     <tr>
-      <td>{user.id}</td>
       <td>{user.email}</td>
-      <td>{user.admin.toString()}</td>
+      <td>{role}</td>
       <td><Button bsStyle="warning" bsSize="small">Make Admin</Button></td>
       <td><Button bsStyle="danger" bsSize="small">Delete User</Button></td>
     </tr>
