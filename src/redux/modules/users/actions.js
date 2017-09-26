a528a7e6be0a53ddd61299bb269feafd284ae2d6
Nathan Robbert <admin@bigrocketgames.com>
2017-09-25 22:35:43 -0700

    Added upgrade and downgrade actions to correspond with admin button.

--- src/redux/modules/users/actions.js
+++ src/redux/modules/users/actions.js
@@ -14,10 +14,42 @@ export const getUsersSuccess = users => {
 
 // Async actions - connect to Rails API
 
-export const getUsers = (router) => {
+export const getUsers = () => {
   return dispatch => {
     return fetch(`${API_URL}/users`, {
       method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer: " + localStorage.getItem('team.schedule.token')
+      }
+    })
+      .then(response => response.json())
+      .then(users => dispatch(getUsersSuccess(users)))
+      .catch(error => console.log(error));
+  }
+}
+
+export const upgradeUser = (userId) => {
+  return dispatch => {
+    return fetch(`${API_URL}/users/${userId}/upgrade`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': "Bearer: " + localStorage.getItem('team.schedule.token')
+        }
+    })
+      .then(response => response.json())
+      .then(users => dispatch(getUsersSuccess(users)))
+      .catch(error => console.log(error));
+  }
+}
+
+export const downgradeUser = (userId) => {
+  return dispatch => {
+    return fetch(`${API_URL}/users/${userId}/downgrade`, {
+      method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
