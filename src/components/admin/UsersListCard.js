1d80752766777eff704541849e206f5a55328520
Nathan Robbert <admin@bigrocketgames.com>
2017-09-25 19:45:34 -0700

    Added admin column to show if user is admin or not.

--- src/components/admin/UsersListCard.js
+++ src/components/admin/UsersListCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const UsersListCard = ({user}) => (
-  <tr>
-    <td>{user.id}</td>
-    <td>{user.email}</td>
-    <td><Button bsStyle="warning" bsSize="small">Make Admin</Button></td>
-    <td><Button bsStyle="danger" bsSize="small">Delete User</Button></td>
-  </tr>
-)
+const UsersListCard = ({user}) => {
+  return (
+    <tr>
+      <td>{user.id}</td>
+      <td>{user.email}</td>
+      <td>{user.admin.toString()}</td>
+      <td><Button bsStyle="warning" bsSize="small">Make Admin</Button></td>
+      <td><Button bsStyle="danger" bsSize="small">Delete User</Button></td>
+    </tr>
+  )
+}
 
 export default UsersListCard;
