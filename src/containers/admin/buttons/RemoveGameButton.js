9a47584259d43d67060f42c6b7c1ba37d54ee611
Nathan Robbert <admin@bigrocketgames.com>
2017-10-01 12:26:47 -0700

    connected deleteGame action and dispatch that action upon clicking of delete game button.

--- src/containers/admin/buttons/RemoveGameButton.js
+++ src/containers/admin/buttons/RemoveGameButton.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
+
+import { deleteGame } from '../../../redux/modules/games/actions';
 
 class RemoveGameButton extends Component {
   constructor(props) {
@@ -9,14 +12,14 @@ class RemoveGameButton extends Component {
   }
 
   deleteGame(e) {
-
+    this.props.deleteGame(e.target.id);
   }
 
   render() {
     return(
-      <Button bsSize="small" bsStyle="danger" id={this.props.game.id}>Delete Game</Button>
+      <Button bsSize="small" bsStyle="danger" id={this.props.game.id} onClick={this.deleteGame}>Delete Game</Button>
     )
   }
 }
 
-export default RemoveGameButton
+export default connect(null, {deleteGame})(RemoveGameButton)
