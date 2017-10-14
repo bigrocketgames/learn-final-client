abd8a08faab391bb7b6d6fea632068f38ea97477
Nathan <admin@bigrocketgames.com>
2017-10-14 15:56:38 -0700

    Mapped state to props to display the error message if there was a username/password error.

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import LoginForm from './LoginForm';
 
 class Login extends Component {
 
   render() {
+    let errorBanner = null;
+    if (this.props.error) {
+      errorBanner = <div className="container ERROR">{this.props.error}</div>
+    }
     return (
       <div className="login container">
         <h2 className="text-center">Sign In</h2>
+        {errorBanner}
         <LoginForm/>
       </div>
     )
@@ -15,4 +21,8 @@ class Login extends Component {
 
 }
 
-export default Login
+const mapStateToProps = (state) => {
+  return { error: state.auth.error };
+};
+
+export default connect(mapStateToProps)(Login)
