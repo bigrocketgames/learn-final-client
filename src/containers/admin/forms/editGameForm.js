c34a999914d4878a6a7c72e4d7ee3bb16a819528
Nathan <admin@bigrocketgames.com>
2017-10-15 14:18:34 -0700

    Removed connect from the form as it doesn't need to connect.  Modified placeholders and values to access the proper props.

--- src/containers/admin/forms/editGameForm.js
+++ src/containers/admin/forms/editGameForm.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 
 import { updateGame } from '../../../redux/modules/games/actions';
 
 const handleEditGame = (values, dispatch, props) => {
   if(values.game_time === undefined) {
-    values.game_time = props.games.game_time
+    values.game_time = props.game.game_time
   }
   if(values.location === undefined) {
-    values.location = props.games.location
+    values.location = props.game.location
   }
-  dispatch(updateGame(values, props.games.id));
+  dispatch(updateGame(values, props.game.id));
 }
 
 let EditGameForm = props => {
   const { handleSubmit, pristine, reset, submitting } = props
   return (
     <form onSubmit={handleSubmit(handleEditGame)} className="editGameForm">
-      <div className="container">
-        <h2 className="text-center">Edit Game</h2>
+      <div>
+        <h2 className="text-center">Edit Game:</h2>
+        <h4>{props.game.away_team.fullname} @ {props.game.home_team.fullname}</h4>
       </div>
       <div>
         <label htmlFor="game_time">Game Time: </label>
-        <Field name="game_time" component="input" type="text" placeholder={props.games.game_time}/>
+        <Field name="game_time" component="input" type="text" placeholder={props.game.game_time}/>
       </div>
       <div>
         <label htmlFor="location">Game Location:</label>
-        <Field name="location" component="input" type="text" placeholder={props.games.location}/>
+        <Field name="location" component="input" type="text" placeholder={props.game.location}/>
       </div>
       <div>
         <button type="submit" disabled={pristine || submitting}>
@@ -44,12 +44,5 @@ let EditGameForm = props => {
 EditGameForm = reduxForm({
   form: 'editGame'
 })(EditGameForm)
-
-EditGameForm = connect(
-  state => ({
-    games: state.games,
-    teams: state.teams
-  })
-)(EditGameForm)
 
 export default EditGameForm;
