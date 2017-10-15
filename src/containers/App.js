ab1962fc7242b3f193366b9987fad4e316e18a3e
Nathan <admin@bigrocketgames.com>
2017-10-15 14:14:52 -0700

    Removed NavLink since it is unused.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route, Switch, NavLink } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import history from '../history';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
