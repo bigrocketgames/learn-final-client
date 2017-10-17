a65b0c7e6e67fd6e5ebf04fc8b890981e235397f
Nathan <admin@bigrocketgames.com>
2017-10-16 19:19:06 -0700

    Added working button and span to increment the likes of each userTeam and display it next to the button.
    Did this without saving them to API backend, just local component state.

--- src/components/UserTeamsCard.js
+++ src/components/UserTeamsCard.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-const UserTeamsCard = ({userTeam}) => {
-  return (
-    <h3>
-      <Link to={`/teams/${userTeam.team.id}/schedule`}>{userTeam.team.fullname}</Link>
-    </h3>
-  )
+// const UserTeamsCard = ({userTeam}) => {
+//   let likes = 0;
+
+//   const addOne = () => {
+//     console.log("clicked");
+//     return likes++;
+//   }
+
+//   return (
+//     <div>
+//       <h3>
+//         <Link to={`/teams/${userTeam.team.id}/schedule`}>{userTeam.team.fullname}</Link>
+//       </h3>
+//       <button onClick={addOne}>Like</button>
+//       <span>{likes}</span>
+//     </div>
+//   )
+// }
+
+class UserTeamsCard extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      likes: 0,
+    };
+  }
+
+  
+  addOne = () => {
+    this.setState({
+      likes: ++this.state.likes,
+    })
+  }
+
+  render(){
+    return (
+      <div>
+      <h3>
+        <Link to={`/teams/${this.props.userTeam.team.id}/schedule`}>{this.props.userTeam.team.fullname}</Link>
+        </h3>
+        <button id={this.props.userTeam.id} onClick={this.addOne}>Like</button>
+        <span id={this.props.userTeam.id}>  {this.state.likes}</span>
+      </div>
+    )
+  }
 }
+
+
 
 export default UserTeamsCard;
