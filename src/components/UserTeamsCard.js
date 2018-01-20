a1c4df40f3e8ea6b5277c9b02ece2d1caf41ef84
bigrocketgames <admin@bigrocketgames.com>
2018-01-20 15:16:36 -0800

    chore:  Changed back to stateless component as I won't be using it to keep track of likes on the homepage.

--- src/components/UserTeamsCard.js
+++ src/components/UserTeamsCard.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 
-import { addLike } from '../redux/modules/userteams/actions';
+const UserTeamsCard = ({userTeam}) => {
+  // constructor(props){
+  //   super(props)
 
-class UserTeamsCard extends Component {
-  constructor(props){
-    super(props)
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = (e) => {
-    e.preventDefault();
-    const userTeamID = Number(e.target.id);
-    this.props.addLike(userTeamID);
+  // handleClick = (e) => {
+  //   e.preventDefault();
+  //   const userTeamID = Number(e.target.id);
+  //   this.props.addLike(userTeamID);
     
-  }
+  // }
 
-  render() {
-    const userTeam = this.props.userTeam;
-    return (
-      <div>
-        <h3 className="userTeamLink">
-          <Link to={`/teams/${userTeam.team.id}/schedule`}>{userTeam.team.fullname}</Link>
-        </h3>
-        <Button id={userTeam.id} className="likeButton" bsSize="xsmall" bsStyle="primary" onClick={this.handleClick}>Like</Button>
-        <span>{userTeam.team.likes}</span>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h3 className="userTeamLink">
+        <Link to={`/teams/${userTeam.team.id}/schedule`}>{userTeam.team.fullname}</Link>
+      </h3>
+    </div>
+  )
 }
 
-export default connect(null, { addLike })(UserTeamsCard)
+export default UserTeamsCard
