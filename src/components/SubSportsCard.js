41ac2497ee27d2e46489c795b9e7eb55ef3147c7
bigrocketgames <admin@bigrocketgames.com>
2018-03-04 16:21:13 -0800

    chore:  Added a reminder for the user to login if they want to save their favorite teams and hides the favoriteButton if not logged in.

--- src/components/SubSportsCard.js
+++ src/components/SubSportsCard.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Table } from 'react-bootstrap';
 
 import SubSportTeamsList from '../containers/SubSportsTeamsList';
 
-const SubSportsCard = ({subSport}) => {
+const SubSportsCard = ({subSport, user}) => {
+  let userReminder = null;
+  if (!user.id) {
+    userReminder = <div><p className="text-center ERROR"><Link to={`/login`}>Login</Link> or <Link to={"/signup"}>Signup</Link> to save a team to your favorites!</p><hr /></div>
+  }
   if (subSport.teams !== undefined) {
     if (subSport.teams.length > 0) {
       return (
         <div className="container">
           <h2 className="text-center">{subSport.name} TEAMS</h2>
+          {userReminder}
           <Table striped bordered responsive>
             <thead>
               <tr>
