3fca1e4c679efa61d516780f193a5ad29673031d
Nathan Robbert <admin@bigrocketgames.com>
2018-07-04 20:38:52 -0700

    fix:  Renaming button SFC to avoid naming conflict.

---
+++ src/containers/ButtonComp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+const ButtonComp = (props) => {
+  console.log(props)
+  return(
+    <Button bsSize={`props.btnSize`} bsStyle={`props.btnStyle`} onClick={props.handleClick} id={props.id}>{props.label}</Button>
+  )
+}
+
+export default ButtonComp
