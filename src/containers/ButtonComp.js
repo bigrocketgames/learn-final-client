b214b56ac5b8ae46cc8f32a7520cc1977cb657fe
Nathan Robbert <admin@bigrocketgames.com>
2018-07-06 21:06:14 -0700

    fix:  Button component now properly using the props it receives.

--- src/containers/ButtonComp.js
+++ src/containers/ButtonComp.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {Button} from 'react-bootstrap';
 
 const ButtonComp = (props) => {
-  console.log(props)
   return(
-    <Button bsSize={`props.btnSize`} bsStyle={`props.btnStyle`} onClick={props.handleClick} id={props.id}>{props.label}</Button>
+    <Button bsSize={props.btnSize} bsStyle={props.btnStyle} onClick={props.handleClick} id={props.id}>{props.label}</Button>
   )
 }
 
