2223c867c5678e01dd089d1352b0a35e837de653
bigrocketgames <admin@bigrocketgames.com>
2018-07-14 08:37:20 -0700

    feat:  Link to game chat rooms is now working.

--- src/components/ScheduleCard.js
+++ src/components/ScheduleCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 
-const ScheduleCard = (schedule) => {
-  const game_time = new Date(schedule.game.game_time);
+const ScheduleCard = ({game}) => {
+  console.log(game.chatRoomId)
+  const game_time = new Date(game.game_time);
   const options = {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZoneName: 'short'}
   return (
     <tr>
       <td>{game_time.toLocaleDateString("lookup", options)}</td>
-      <td>{schedule.game.away_team.fullname}</td>
+      <td>{game.away_team.fullname}</td>
       <td>@</td>
-      <td>{schedule.game.home_team.fullname}</td>
-      <td>{schedule.game.location}</td>
-      <td>Link to Chat room</td>
+      <td>{game.home_team.fullname}</td>
+      <td>{game.location}</td>
+      <td><Link to={`/chat_rooms/${game.chatRoomId}`}>Chat It Up</Link></td>
     </tr>
   )
 }
