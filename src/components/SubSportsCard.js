8af197e0342873e3647ee711860ff48bef278ec0
bigrocketgames <admin@bigrocketgames.com>
2018-07-29 07:49:06 -0700

    chore:  Updating SubSportsCard and SubSports components for use with new conference format.

--- src/components/SubSportsCard.js
+++ src/components/SubSportsCard.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Table } from 'react-bootstrap';
 
 import SubSportTeamsList from '../containers/SubSportsTeamsList';
 
-const SubSportsCard = ({subSport, user}) => {
-  let userReminder = null;
-  if (!user.id) {
-    userReminder = <div><p className="text-center ERROR"><Link to={`/login`}>Login</Link> or <Link to={"/signup"}>Signup</Link> to save a team to your favorites!</p><hr /></div>
-  }
-  if (subSport.teams !== undefined) {
-    if (subSport.teams.length > 0) {
+const SubSportsCard = ({conference}) => {
+  if (conference.teams !== undefined) {
+    if (conference.teams.length > 0) {
       
-      const teams = subSport.teams.sort(function(a, b) {
+      const teams = conference.teams.sort(function(a, b) {
         if (a.fullname > b.fullname) {
           return 1;
         }else if (a.fullname < b.fullname) {
@@ -23,9 +18,8 @@ const SubSportsCard = ({subSport, user}) => {
       });
 
       return (
-        <div className="container">
-          <h2 className="text-center">{subSport.name} TEAMS</h2>
-          {userReminder}
+        <div>
+          <h2 className="text-center">{conference.name} TEAMS</h2>
           <Table striped bordered responsive>
             <thead>
               <tr>
@@ -38,21 +32,23 @@ const SubSportsCard = ({subSport, user}) => {
               {teams.map(team => <SubSportTeamsList key={team.id} team={team} />)}
             </tbody>
           </Table>
+          <hr />
         </div>
       )
     } else {
       return (
-        <div className="container">
-          <h2 className="text-center">{subSport.name} TEAMS</h2>
+        <div>
+          <h2 className="text-center">{conference.name} TEAMS</h2>
           <h5>Unfortunately, there are no teams to show for this league at this time.</h5>
           <h5>We are working to update this as soon as possible.</h5>
+          <hr />
         </div>
       )
     }
   } else {
     return (
-      <div className="container">
-        <h2 className="text-center">{subSport.name} TEAMS</h2>
+      <div>
+        <h2 className="text-center">{conference.name} TEAMS</h2>
         <h5>Unfortunately, there are no teams to show for this league at this time.</h5>
       </div>
     )
--- src/containers/SubSports.js
+++ src/containers/SubSports.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 
 import { getSubSport } from '../redux/modules/subsports/actions';
 import SubSportsCard from '../components/SubSportsCard';
@@ -23,8 +24,19 @@ class SubSports extends Component {
   }
 
   render() {
+    const {user, sub_sports} = this.props
+    let userReminder = null;
+    if (!user.id) {
+      userReminder = <div><p className="text-center ERROR"><Link to={`/login`}>Login</Link> or <Link to={"/signup"}>Signup</Link> to save a team to your favorites!</p><hr /></div>
+    }
+
     return(
-        <SubSportsCard subSport={this.props.sub_sports} user={this.props.user} />
+      <div className="container">
+        <h2 className="text-center">{sub_sports.name} TEAMS</h2>
+        <hr />
+        {userReminder}
+        {sub_sports.conferences && sub_sports.conferences.map(conference => <SubSportsCard key={conference.id} conference={conference} />)}
+      </div>
     )
   }
 }
